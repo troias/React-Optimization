be352dd953300b01a54558a9f457b4e9971b8f31
troy <hunter007nz@gmail.com>
2022-07-04 20:29:07 +1200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React, { useState, useCallback, useMemo } from 'react';
-import Button from './components/UI/Button/Button'
-import DemoOutput from './components/Demo/DemoOutput'
-import './App.css';
-import DemoList from './components/Demo/DemoList'
-
-
+import React, { useState, useCallback, useMemo } from "react"
+import Button from "./components/UI/Button/Button"
+import DemoOutput from "./components/Demo/DemoOutput"
+import "./App.css"
+import DemoList from "./components/Demo/DemoList"
 
 function App() {
-  const items = [ 5 ,8,4,3,8,4,]
+  const items = [5, 8, 4, 3, 8, 4]
 
   const [show, setShow] = useState(false)
   const [allowToggle, setAllowToggle] = useState(false)
   console.log("App, app running")
 
-  const buttonHandler = useCallback(() => { 
+  const buttonHandler = useCallback(() => {
     if (allowToggle) {
       setShow((prevState) => !prevState)
     }
-  },[allowToggle])
+  }, [allowToggle])
 
-  const toggleHandler = useCallback(() => { 
-    setAllowToggle(true)
-  },[])
+  const toggleHandler = useCallback(() => {
+    setAllowToggle((prevState) => !prevState)
+  }, [])
 
   return (
     <div className="app">
-      <h1 >Starting Application</h1>
-      <DemoList title={"List"} items={useMemo(() => items, [])
-      } />
-      <Button style={{
-        marginBottom: "10px"
-      }} onClick={toggleHandler}>Allow Toggle</Button>
+      <h1>Starting Application</h1>
+      <DemoList title={"List"} items={useMemo(() => items, [])} />
 
-      <Button onClick={buttonHandler}>Toggle Paragraph</Button>
-   
+      <Button
+        style={{
+          marginBottom: "10px",
+        }}
+        onClick={toggleHandler}
+      >
+        {allowToggle ? "Disable" : "Enable"}
+      </Button>
+
+      <Button onClick={buttonHandler}>{show ? "show" : "hide"}</Button>
     </div>
-  );
+  )
 }
 
 export default App
